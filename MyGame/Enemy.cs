178f66a6796256fbfd0303868547a883e4ccd414
NelsonSalvador <nelsonsalvadorzx@gmail.com>
2020-04-23 10:42:07 +0100

    Ex 1

---
+++ MyGame/Enemy.cs
@@ -0,0 +1,45 @@
+namespace MyGame
+{
+    class Enemy
+    {
+        private string name;
+        private float health;
+        private float shield;
+
+        public Enemy(string name)
+        {
+            this.name = name;
+            health = 100;
+            shield = 0;
+        }
+        public Enemy(string name, float health, float shield)
+        {
+            
+            this.health = health;
+            this.shield = shield;
+        }
+
+        public string Getname() => name;
+
+        public float GetTotPower() => health + shield;
+
+        public float GetHealth() => health;
+        public float GetShield() => shield;
+
+        public void SetName(string name)
+        {
+            this.name = name.Trim();
+        }
+
+        public void TakeDamage(float damage)
+        {
+            shield -= damage;
+            if (shield < 0)
+            {
+                float damageLeft = -shield;
+                health -= damageLeft;
+                if (health < 0) health = 0;
+            }
+        }
+    }
+}
---
+++ MyGame/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Enemy enemy1 = new Enemy("Loki");
+            Enemy enemy2 = new Enemy("Thor", 100, 100);
+            Console.WriteLine(enemy1.Getname());
+        }
+    }
+}
